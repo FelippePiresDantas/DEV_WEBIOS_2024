7c67adc779c770ec6271adf3af81a46445481b10
FelippePiresDantas <felippepiresdantas@gmail.com>
2024-05-08 21:45:16 -0300

    aula 02JS

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,22 @@
+//obj console
+console.log("Olá MÉEEEEEESSIIIIIIIIIIII");
+
+//variáveis
+
+var _felippe = 8;//PODE SER REDECLARADO
+
+console.log ("O Messi tem ",_felippe, " bolas de ouro.");
+
+//LET NAO PODE SER REDECLARADO
+//CONST NAO PODE SER ALTERADA ATÉ O FIM DO PROGRAMA
+
+//console.clear();
+
+//NaN quer dizer que nao é um número (not a number)
+//infinity (número muito grande para ser mostrado)
+//-infinity (número muito grande negativo para ser mostrado)
+
+//string = texto
+
+// == compara se é igual
+// =!
