1926b734746e5555ad54ad7bb17f8ae06e04dcd7
FelippePiresDantas <felippepiresdantas@gmail.com>
2024-05-12 22:05:33 -0300

    exercicio casa

---
+++ JS/JS02-Variaveis/ExercicioCasa/main.js
@@ -0,0 +1,11 @@
+console.log ("O aluno Cristian teve 4 notas para passar de ano")
+var n1 = 9
+console.log ("A primeira nota foi de:" , n1)
+var n2 = 6
+console.log ("A segunda foi:" , n2)
+var n3 = 7
+console.log ("A terceira foi:" , n3)
+var n4 = 2
+console.log ("A quarta foi:" , n4)
+var m = (n1 + n2 + n3 + n4) / 4
+console.log ("A média de Cristian foi de:" , m)
