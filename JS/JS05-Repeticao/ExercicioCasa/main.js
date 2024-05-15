074c07695bbbcff775b3923db94866d01ed1694e
FelippePiresDantas <felippepiresdantas@gmail.com>
2024-05-15 20:54:31 -0300

    aula JS05 repeticao

---
+++ JS/JS05-Repeticao/ExercicioCasa/main.js
@@ -0,0 +1,5 @@
+var c = 0
+
+while (c < 11)
+    {console.log ("3 x" , c , "=" , 3 * c)
+     c++};
---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,20 @@
+// while (enquanto)
+var n = 0
+
+while (n < 10) 
+    {n = n+1;
+    console.log (n);}
+
+console.clear ();
+
+// do while (faça enquanto)
+var num = 0
+do {num++;
+    console.log (num)}
+while (num < 10);
+
+console.clear ();
+
+// for (para)
+for (var nu = 0; nu < 11; nu++)
+    {console.log (nu)}
