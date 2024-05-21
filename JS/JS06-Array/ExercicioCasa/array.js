3fd9a7b2194182cd49aae598d2f49a7020623b7b
FelippePiresDantas <felippepiresdantas@gmail.com>
2024-05-20 21:51:47 -0300

    Aula js06

---
+++ JS/JS06-Array/ExercicioCasa/array.js
@@ -0,0 +1,13 @@
+// exercicio 1
+var ar = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995,
+ "React", 2013, "Java", 1995];
+
+ console.log (ar);
+ console.log (ar.length);
+ console.log (ar[6]);
+
+ var transf = (ar.toString());
+ console.log (typeof(transf));
+
+ console.log (ar.join("/"));
+ console.log (ar.unshift ("IOS"));
